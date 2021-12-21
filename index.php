f4385fb852d7483fe8507b5fcf478fdc335e53e9
mdeias <mdeias@icloud.com>
2021-12-21 17:32:26 +0100

    Stampa paragrafi semplici

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $dates = [
     [
         'title' => 'Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?',
 
-        'par<agraph' => 'Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.
+        'paragraph' => 'Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.
 
         Siamo costantemente al lavoro per garantire un elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.
         
@@ -46,7 +46,7 @@ $dates = [
 
     [
         'title' => 'Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?',
-        'paragrap' => 'In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell URL referrer potrebbero essere disponibili mediante Google Analytics o un API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all esatte parole chiave che hanno determinato il clic su un annuncio.'
+        'paragraph' => 'In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell URL referrer potrebbero essere disponibili mediante Google Analytics o un API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all esatte parole chiave che hanno determinato il clic su un annuncio.'
     ]
 
 ];
@@ -65,6 +65,7 @@ $dates = [
     <main>
         <?php foreach($dates as $data){ ?>
             <h2> <?php echo $data['title']; ?> </h2>
+            <p> <?php echo $data['paragraph']; ?> </p>
         <?php } ?>
     </main>
 </body>
